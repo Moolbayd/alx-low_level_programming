d6d80577609043eabb1572b9e18052bae74685e2
Moolbayd <Idrissi.h09@gmail.com>
2023-05-08 06:55:09 -0700

    0x15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,37 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - read text and print it to POSIX
+ * @filename: name of file char
+ * @letters:number of letters to read size_t
+ * Return: read the file display it or error 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i, y;
-	char *buf;
-	if (!filename)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
+	int fd, rd, wd;
+	char *buff = malloc(sizeof(char) * letters);
+
+	if (filename == NULL)
 	{
-		free(buf);
 		return (0);
 	}
-	buf[i] = '\0';
+
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		return (0);
+	}
+	wd = write(STDOUT_FILENO, buff, rd);
+	if (wd == -1)
+	{
+		return (0);
+	}
 	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	free(buf);
-	return (y);
+	free(buff);
+	return (wd);
 }
